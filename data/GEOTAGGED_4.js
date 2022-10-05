2084c534b84d4097a9c0137e8bbc9f7971049828
corpusmeldon <114963613+corpusmeldon@users.noreply.github.com>
2022-10-05 14:39:06 +0800

    Add files via upload

---
+++ data/GEOTAGGED_4.js
@@ -0,0 +1,1 @@
+var json_GEOTAGGED_4 = {"type":"FeatureCollection","name":"GEOTAGGED_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"Name":"Geotag","folder":"C:\\RLGM\\sAM"},"geometry":{"type":"Point","coordinates":[125.099590309381909,8.831180784236604]}}]}
